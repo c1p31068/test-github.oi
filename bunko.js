ab7f30af695ef0f64ee7010af85a98028f371526
セオドア・ルーズベルト <91302194+c1p31068@users.noreply.github.com>
2023-08-11 23:24:49 +0900

    Update bunko.js

--- bunko.js
+++ bunko.js
@@ -1,3 +1,8 @@
+// ファイルデータ
 const file = document.getElementById("example").files[0];
-
+// フォームデータを作成
+const formData = new FormData();
+// avatarというフィールド名でファイルを追加
+formData.append("avatar", file);
+// アップロード
 fetch(送信先のURL, { method: "POST", body: formData });
