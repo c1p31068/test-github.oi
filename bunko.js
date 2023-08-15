608e720227508e3802c902fb130ce32f9a147ffd
セオドア・ルーズベルト <91302194+c1p31068@users.noreply.github.com>
2023-08-15 21:45:56 +0900

    Delete bunko.js
    フラスクを動かすため

--- bunko.js
+++
@@ -1,8 +0,0 @@
-// ファイルデータ
-const file = document.getElementById("example").files[0];
-// フォームデータを作成
-const formData = new FormData();
-// avatarというフィールド名でファイルを追加
-formData.append("avatar", file);
-// アップロード
-fetch(送信先のURL, { method: "POST", body: formData });
