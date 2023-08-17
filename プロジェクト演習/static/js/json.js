db39183ee96e295811e5ddc8dc45e2a01e72a604
c1p31067 <c1p31067@bunkyo.ac.jp>
2023-08-17 12:04:47 +0900

    d

--- プロジェクト演習/static/js/json.js
+++
@@ -1,9 +0,0 @@
-function setImage(e) {
-  const file = e.target.files;
-  const reader = new FileReader();
-  reader.readAsDataURL(file[0]);
-  reader.onload = () => {
-    const image = document.getElementById('img');
-    image.src = reader.result;
-  };
-}
